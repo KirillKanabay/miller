a9cf6d180e25ed8331d9dca9b35e20ddfd829fff
Kirill Kanabay <kirill.kanabay@gmail.com>
2021-11-01 22:30:15 +0300

    Добавлен адаптив шапки и бургер меню под планшеты

--- scripts/menu-mobile.js
+++ scripts/menu-mobile.js
@@ -1,15 +1,18 @@
 const mobileMenu = document.getElementById("menu-mobile");
 const closeButton = document.getElementById("close-menu-mobile-button");
 const openButton = document.getElementById("open-menu-mobile-button");
+const body = document.body;
 
 const closeMenu = (e)=>{
    e.preventDefault();
    mobileMenu.classList.add("hidden");
+   body.classList.remove("overflow-hidden");
 }
 
 const openMenu = (e) =>{
    e.preventDefault();
    mobileMenu.classList.remove("hidden");
+   body.classList.add("overflow-hidden");
 }
 
 closeButton.addEventListener("click", closeMenu);
