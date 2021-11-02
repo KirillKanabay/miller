115df71ac6f0f21e01d89a91ebab14f4adf3cd3d
Kirill Kanabay <kirill.kanabay@gmail.com>
2021-11-02 18:26:30 +0300

    Восстановлен блок товаров со скидками

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,12 +1,16 @@
+$(document).ready(function () {
+    $('.catalog__carousel').slick({
+        dots: true,
+        Infinity: true,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+    });
+});
 
-$(document).ready(function(){
-    $('.catalog__carousel').slick(
-        {
-            dots: true,
-            Infinity: true,
-            arrows: false,
-            centerMode: true,
-            centerPadding: '50px',
-        }
-    );
-  });
+$(document).ready(function () {
+    $('.discount__list').slick({
+        infinite: false,
+        arrows: false,
+    });
+});
