7b867173ab2788f9dd78b5c96a0377f0f15b2977
Kirill Kanabay <kirill.kanabay@gmail.com>
2021-11-02 16:34:49 +0300

    Реализован слайдер каталога для мобильной версии

--- scripts/menu-mobile.js
+++ scripts/menu-mobile.js
@@ -2,17 +2,18 @@ const mobileMenu = document.getElementById("menu-mobile");
 const closeButton = document.getElementById("close-menu-mobile-button");
 const openButton = document.getElementById("open-menu-mobile-button");
 const body = document.body;
+const pageContainer = document.getElementById("page-container")
 
 const closeMenu = (e)=>{
    e.preventDefault();
    mobileMenu.classList.add("hidden");
-   body.classList.remove("overflow-hidden");
+   pageContainer.classList.remove("hidden");
 }
 
 const openMenu = (e) =>{
    e.preventDefault();
    mobileMenu.classList.remove("hidden");
-   body.classList.add("overflow-hidden");
+   pageContainer.classList.add("hidden");
 }
 
 closeButton.addEventListener("click", closeMenu);
